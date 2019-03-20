757c39c78ee46c7d3c958d03bda2f5e92630b9c7
kalanaelapatha <38855998+kalanaelapatha@users.noreply.github.com>
2019-03-20 15:58:15 +0530

    Complete Sales

--- udhmanagmentsystem/src/main/java/com/UDHFashion/udhmanagmentsystem/service/IBillDAOImpl.java
+++ udhmanagmentsystem/src/main/java/com/UDHFashion/udhmanagmentsystem/service/IBillDAOImpl.java
@@ -1,11 +1,12 @@
 package com.UDHFashion.udhmanagmentsystem.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
 import com.UDHFashion.udhmanagmentsystem.model.Bill;
 import com.UDHFashion.udhmanagmentsystem.util.CommonConstants;
 
@@ -37,11 +38,28 @@ public class IBillDAOImpl implements BillDAO {
 
 	@Override
 	public List<Bill> getAllBill() {
-		   
 		
-		
-		
-		return null;
+		List<Map<String, Object>> rows = jdbcTemplate.queryForList(CommonConstants.GET_ALL_BILL_DETAILS);
+
+		List<Bill> result = new ArrayList<Bill>();
+
+		for (Map<String, Object> row : rows) {
+			Bill bill = new Bill();
+
+			bill.setDate((String) row.get("date"));
+			bill.setCashireId((Integer) row.get("cashireId"));
+			bill.setGrossAmount((Double) row.get("grossAmount"));
+			bill.setNetAmount((Double) row.get("netAmount"));
+			bill.setTotalDiscount((Double)row.get("totalDiscount"));
+			bill.setBalance((Double)row.get("balance"));
+			bill.setNoOfItem((Integer)row.get("noOfItem"));
+
+			System.out.println("Shop Name Debug :  " + bill.getNetAmount());
+
+			result.add(bill);
+		}
+
+		return result;
 	}
 
 	@Override
